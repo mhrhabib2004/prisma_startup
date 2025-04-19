8385510b2a8943e36dd147a2257779eaaaf68ce8
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-04-19 23:58:41 +0600

    up;some

--- src/paiginate_shorting.ts
+++ src/paiginate_shorting.ts
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 const paginateShorting =async ()=>{
     const offfsetpeginate = await prisma.post.findMany({
-        
+        skip : 3,
+        take:3
     })
+    console.log(offfsetpeginate);
 }
+paginateShorting()
