20713dcbd83b0dfed92e2db9f0609fd88a3f8b2e
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-04-20 23:18:52 +0600

    up: some  parisma

--- src/paiginate_shorting.ts
+++ src/paiginate_shorting.ts
@@ -3,10 +3,26 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const paginateShorting =async ()=>{
-    const offfsetpeginate = await prisma.post.findMany({
+    // const offfsetpeginate = await prisma.post.findMany({
+    //     skip : 3,
+    //     take:3
+    // })
+    // console.log(offfsetpeginate);
+    const curssortpeginate = await prisma.post.findMany({
         skip : 3,
-        take:3
+        take:3,
+        cursor : {
+            id : 10
+        }
     })
-    console.log(offfsetpeginate);
+    // console.log(curssortpeginate);
+    // shorting data
+
+    const shorting =  await prisma.post.findMany({
+        orderBy : {
+            titel : "desc"
+        }
+    })
+    console.log(shorting);
 }
 paginateShorting()
